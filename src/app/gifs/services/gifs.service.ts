70c6d48114633c0727440080ac173e6a9c7001e6
Alvaro Marmol <amr000.edu@innovasur.com>
2024-10-08 08:23:26 +0200

    Buscador de gifs desarrollado en angular

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  public gifList:Gif[]=[];
+
+  private _tagsHistory: string[] =[];
+  private apiKey:string='KPMFmjoydR53dgry5uJnQlnP3sgYo4Gw';
+  private serviceUrl='https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {
+
+    this.loadLocalStorage();
+
+    console.log("Gifs service ready")
+
+   }
+
+  get tagsHistory(){
+    return [...this._tagsHistory];
+  }
+  private organizeHistory(tag:string)
+  {
+    tag = tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag))//Si el tag ya esta
+    {
+      this._tagsHistory=this._tagsHistory.filter((oldTag)=>oldTag !== tag)//Lo elimino
+    }
+    this._tagsHistory.unshift(tag);//Y lo inserto al principio
+    this._tagsHistory=this.tagsHistory.splice(0,10);//Limitamos la busqueda a 10
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void
+  {
+    localStorage.setItem('history',JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStorage():void
+  {
+    if(!localStorage.getItem('history')) return;
+
+    this._tagsHistory=JSON.parse(localStorage.getItem('history')!)
+
+
+    if(this._tagsHistory.length ===0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  searchTag(tag:string):void
+  {
+    if(tag.length ===0) return;
+
+    this.organizeHistory(tag)
+    const params = new HttpParams()
+    .set('api_key',this.apiKey)
+    .set('limit','10')
+    .set('q',tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
+    .subscribe(resp =>{
+      console.log(resp.data);
+      this.gifList = resp.data;
+
+
+    })
+
+
+   // fetch('https://api.giphy.com/v1/gifs/search?api_key=KPMFmjoydR53dgry5uJnQlnP3sgYo4Gw&q=Valorant&limit=10')
+    //.then(resp => resp.json())
+    //.then(data => console.log(data))
+
+
+
+  }
+
+
+
+
+}
